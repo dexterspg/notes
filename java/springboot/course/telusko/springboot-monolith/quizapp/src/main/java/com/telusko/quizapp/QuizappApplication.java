637055b557ebec629095da818c4ab772b81402c9
Dexter Pagkaliwangan <dexterspg@gmail.com>
2025-01-15 20:47:06 -0500

    added monolith quizapp

---
+++ java/springboot/course/telusko/springboot-monolith/quizapp/src/main/java/com/telusko/quizapp/QuizappApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizappApplication {
+    //uses monolith
+	public static void main(String[] args) {
+		SpringApplication.run(QuizappApplication.class, args);
+	}
+
+}
---
+++ java/springboot/course/telusko/springboot-monolith/quizapp/src/main/java/com/telusko/quizapp/dao/QuizDao.java
@@ -0,0 +1,12 @@
+package com.telusko.quizapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.telusko.quizapp.model.Quiz;
+
+@Repository
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+
+    
+}
